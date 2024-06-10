d05d7085cf4d334764bb354feb20aa8b39b2459d
Robert Kaurin <robert@netgen.io>
2024-06-10 23:34:15 +0200

    add image form field and some bootstrap

--- src/App/Form/RecipeType.php
+++ src/App/Form/RecipeType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Form;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -15,11 +16,32 @@ final class RecipeType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options): void
   {
       $builder->add(
+          'image',
+          FileType::class,
+          [
+              'required' => false,
+              'label' => 'Image',
+              'label_attr' => [
+                'class' => 'col-form-label col-3',
+              ],
+              'attr' => [
+                'class' => 'form-control',
+              ]
+          ],
+      );
+
+      $builder->add(
           'name',
           TextType::class,
           [
               'required' => true,
               'label' => 'Recipe name',
+              'label_attr' => [
+                'class' => 'col-form-label col-3',
+              ],
+              'attr' => [
+                'class' => 'form-control',
+              ]
           ],
       );
 
@@ -29,6 +51,12 @@ final class RecipeType extends AbstractType
           [
               'required' => false,
               'label' => 'Recipe description',
+              'label_attr' => [
+                'class' => 'col-form-label col-3',
+              ],
+              'attr' => [
+                'class' => 'form-control',
+              ]
           ],
       );
   }
